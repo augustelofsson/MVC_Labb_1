8becc2badb5eb921fa9b541fa1fc77e2b277295d
augustelofsson <55317290+augustelofsson@users.noreply.github.com>
2020-04-14 12:57:33 +0200

    removed comment

--- MVC_Labb_1/Models/IGetIpAddressService.cs
+++ MVC_Labb_1/Models/IGetIpAddressService.cs
@@ -21,20 +21,10 @@ namespace MVC_Labb_1.Models
         }
         string IGetIpAddressService.GetIpAddress()
         {
-            
-            //string ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
             string ipAddress = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
 
-            //if (!string.IsNullOrEmpty(ipAddress))
-            //{
-            //    string[] addresses = ipAddress.Split(',');
-            //    if (addresses.Length != 0)
-            //    {
-            //        return addresses[0];
-            //    }
-            //}
             return ipAddress;
-            //return context.Request.ServerVariables["REMOTE_ADDR"];
+            
         }
     }
 }
